6dde5fd93324b607e731a2892553a91d1dd131dc
Evin <ztc280883874@163.com>
2024-12-11 23:59:11 +0800

    init

---
+++ bart-user/src/main/java/com/bart/user/BartUserApplication.java
@@ -0,0 +1,12 @@
+package com.bart.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BartUserApplication
+{
+    public static void main(String[] args) {
+        SpringApplication.run(BartUserApplication.class, args);
+    }
+}
